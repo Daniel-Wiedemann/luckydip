e8c3529edeceb1fddbd4f4fb923ab6597dd1c754
Daniel Wiedemann <dwiedemann@gmx.net>
2022-01-27 12:17:10 +0100

    add grid and colors, change copy

--- api/RandomNumbers.js
+++ api/RandomNumbers.js
@@ -1,24 +1,22 @@
 function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
 function randomIntInRange(min, max, n = 1) {
-  const arr = Array(max - min + 1);
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = min++;
-  }
-  console.log('arr: ', arr);
-  console.log('arr shuffled: ', shuffle(arr));
-  console.log('arr 2: ', arr);
-  return arr.slice(0, 6);
-  return Array.from(
-    { length: n },
-    () => Math.floor(Math.random() * (max - min + 1)) + min
-  );
+    const arr = Array(max - min + 1);
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = min++;
+    }
+    shuffle(arr);
+    return arr.slice(0, 6);
+    //   return Array.from(
+    //     { length: n },
+    //     () => Math.floor(Math.random() * (max - min + 1)) + min
+    //   );
 }
 
 export { randomIntInRange };
--- components/DipNumbers.js
+++ components/DipNumbers.js
@@ -1,41 +1,47 @@
 import { randomIntInRange } from 'api/RandomNumbers';
 import { useEffect, useState } from 'react';
 import {
-  Button,
-  Grid,
-  GridColumn,
-  GridRow,
-  Statistic,
-  StatisticGroup,
-  StatisticValue,
+    Button,
+    Grid,
+    GridColumn,
+    GridRow,
+    Statistic,
+    StatisticGroup,
+    StatisticValue,
 } from 'semantic-ui-react';
 
 export default function DipNumbers() {
-  const [numberList, setNumberList] = useState([]);
-  useEffect(() => setNumberList(randomIntInRange(1, 49, 6)), []);
-  function generateDip() {
-    setNumberList(randomIntInRange(1, 49, 6));
-  }
-  return (
-    <>
-      <Grid centered>
-        <GridRow centered columns={8} color="grey">
-          <StatisticGroup>
-            {numberList
-              .sort((a, b) => a - b)
-              .map((i, j) => (
-                <Statistic color="yellow" key={j}>
-                  <StatisticValue>{i}</StatisticValue>
-                </Statistic>
-              ))}
-          </StatisticGroup>
-        </GridRow>
-        <GridColumn centered>
-          <Button onClick={generateDip} color="yellow" primary>
-            Dip
-          </Button>
-        </GridColumn>
-      </Grid>
-    </>
-  );
+    const [numberList, setNumberList] = useState([]);
+    useEffect(() => setNumberList(randomIntInRange(1, 49, 6)), []);
+    function generateDip() {
+        setNumberList(randomIntInRange(1, 49, 6));
+    }
+    return (
+        <>
+            <Grid
+                textAlign="center"
+                style={{ height: '100vh', backgroundColor: 'grey' }}
+                verticalAlign="middle"
+            >
+                <GridColumn color="grey">
+                    <GridRow centered columns={8} color="grey">
+                        <StatisticGroup>
+                            {numberList
+                                .sort((a, b) => a - b)
+                                .map((i, j) => (
+                                    <Statistic color="yellow" key={j}>
+                                        <StatisticValue>{i}</StatisticValue>
+                                    </Statistic>
+                                ))}
+                        </StatisticGroup>
+                    </GridRow>
+                    <GridRow>
+                        <Button onClick={generateDip} color="yellow" primary>
+                            Dip Dip Dip Hooray
+                        </Button>
+                    </GridRow>
+                </GridColumn>
+            </Grid>
+        </>
+    );
 }
